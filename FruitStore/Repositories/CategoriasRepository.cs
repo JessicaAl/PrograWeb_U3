994e9a45de8352eea3f4e1388780824f33af89d8
JessicaAl <70554611+JessicaAl@users.noreply.github.com>
2020-10-25 04:14:23 -0600

    Add files via upload

---
+++ FruitStore/Repositories/CategoriasRepository.cs
@@ -0,0 +1,30 @@
+﻿using FruitStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FruitStore.Repositories
+{
+    public class CategoriasRepository : Repository<Categorias>
+    {
+        public CategoriasRepository(fruteriashopContext context) : base(context)
+        {
+
+        }
+
+        public override bool Validate(Categorias entidad)
+        {
+            if (string.IsNullOrWhiteSpace(entidad.Nombre))
+            {
+                throw new Exception("No escribió el nombre de la categoría");
+            }
+
+            if (Context.Categorias.Any(x => x.Nombre == entidad.Nombre && x.Id != entidad.Id))
+            {
+                throw new Exception("Ya existe el nombre de esa categoría.");
+            }
+            return true;
+        }
+    }
+}
---
+++ FruitStore/Services/CategoriaService.cs
@@ -0,0 +1,24 @@
+﻿using FruitStore.Models;
+using FruitStore.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FruitStore.Services
+{
+    public class CategoriaService
+    {
+        public List<Categorias> Categorias { get; set; }
+
+        public CategoriaService()
+        {
+          
+            using (fruteriashopContext context = new fruteriashopContext())
+            {
+                Repository<Categorias> repos = new Repository<Categorias>(context);
+                Categorias = repos.GetAll().Where(x => x.Eliminado == 0).OrderBy(x => x.Nombre).ToList();
+            }
+        }
+    }
+}
